4b1a460b17e8b4f84086e02bee9a44d0f32c9a8f
Nicolas Fortun <17000404@e008m03.istic.univ-rennes1.fr>
2016-09-20 12:52:25 +0200

    Mise en place du projet

---
+++ dm_compil/src/dm_compil/Test.java
@@ -0,0 +1,14 @@
+package dm_compil;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+
+public class Test {
+   public static void main(String[] args) throws Exception {
+       ANTLRInputStream input = new ANTLRInputStream(System.in);
+       ExprLexer lexer = new ExprLexer(input);
+       CommonTokenStream tokens = new CommonTokenStream(lexer);
+       ExprParser parser = new ExprParser(tokens);
+       parser.prog();
+   }
+}
